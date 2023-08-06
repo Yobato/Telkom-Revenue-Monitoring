7c16480fe4d31ba61ecf8c2561dea61b70c392e0
robithnr <robithnaufal@gmail.com>
2023-08-07 02:38:54 +0700

    Update Dashboard Manager

---
+++ resources/views/manager/partials/sidebar.blade.php
@@ -0,0 +1,52 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html" class="logo d-flex align-items-center">
+                <img src="{{ asset('favicon.ico') }}" alt="">
+                <span class="d-none d-lg-block">Telkom Akses</span>
+            </a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+          <img src="{{ asset('favicon.ico') }}" alt="">
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"><i class="bi bi-bar-chart-fill"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::route()->getName() == 'admin.dashboard' ? ' active' : '' }}'>
+                        <a class="nav-link"
+                            href="{{ route('manager-gpm') }}">Gross Profit Margin</a>
+                    </li>
+                    <li class="{{ Request::route()->getName() == 'admin.dashboard' ? ' active' : '' }}">
+                        <a class="nav-link"
+                            href="{{ route('manager-cogs') }}">Cost of Goods Sold</a>
+                    </li>
+                    <li class="{{ Request::route()->getName() == 'admin.dashboard' ? ' active' : '' }}">
+                        <a class="nav-link"
+                            href="{{ route('manager-revenue') }}">Revenue</a>
+                    </li>
+                    <li class="{{ Request::route()->getName() == 'admin.dashboard' ? ' active' : '' }}">
+                        <a class="nav-link"
+                            href="{{ route('manager-kkp') }}">KKP Operasional</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#"
+                    class="nav-link has-dropdown"><i class="bi bi-pie-chart-fill"></i><span>Reporting</span></a>
+                <ul class="dropdown-menu">
+                    <li class='{{ Request::route()->getName() == 'commerce.dashboard' ? ' active' : '' }}'>
+                        <a class="nav-link"
+                            href="{{ route('commerce-cogs') }}">COGS</a>
+                    </li>
+                    <li class="{{ Request::route()->getName() == 'commerce.dashboard' ? ' active' : '' }}">
+                        <a class="nav-link"
+                            href="{{ route('commerce-revenue') }}">Revenue</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</div>
