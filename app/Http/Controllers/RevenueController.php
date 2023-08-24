35401d1e68c5db7e118c35335911f708387b04f3
Rahanug <rahanug@gmail.com>
2023-08-24 14:16:15 +0700

    Unclosed bracket

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -13,73 +13,73 @@ class RevenueController extends Controller
     //
     public function index()
 
-{
-    $tahunData = Target::distinct()->get(['tahun']);
     {
-        $commerceData = DB::table('laporan_commerce')
-            ->select(
-                DB::raw('YEAR(tanggal) as year'),
-                DB::raw('MONTH(tanggal) as month'),
-                DB::raw('SUM(nilai) as total_nilai')
-            )
-            ->where('jenis_laporan', '=', 'COGS')
-            ->groupBy('year', 'month')
-            ->orderBy('year', 'asc')
-            ->orderBy('month', 'asc')
-            ->get();
+        $tahunData = Target::distinct()->get(['tahun']); {
+            $commerceData = DB::table('laporan_commerce')
+                ->select(
+                    DB::raw('YEAR(tanggal) as year'),
+                    DB::raw('MONTH(tanggal) as month'),
+                    DB::raw('SUM(nilai) as total_nilai')
+                )
+                ->where('jenis_laporan', '=', 'COGS')
+                ->groupBy('year', 'month')
+                ->orderBy('year', 'asc')
+                ->orderBy('month', 'asc')
+                ->get();
 
-        $revenueData = DB::table('laporan_commerce')
-            ->select(
-                DB::raw('YEAR(tanggal) as year'),
-                DB::raw('MONTH(tanggal) as month'),
-                DB::raw('SUM(nilai) as total_nilai')
-            )
-            ->where('jenis_laporan', '=', 'REVENUE')
-            ->groupBy('year', 'month')
-            ->orderBy('year', 'asc')
-            ->orderBy('month', 'asc')
-            ->get();
+            $revenueData = DB::table('laporan_commerce')
+                ->select(
+                    DB::raw('YEAR(tanggal) as year'),
+                    DB::raw('MONTH(tanggal) as month'),
+                    DB::raw('SUM(nilai) as total_nilai')
+                )
+                ->where('jenis_laporan', '=', 'REVENUE')
+                ->groupBy('year', 'month')
+                ->orderBy('year', 'asc')
+                ->orderBy('month', 'asc')
+                ->get();
 
-        $targetData = DB::table('target')
-            ->select(
-                DB::raw('tahun as year'),
-                DB::raw('bulan as month'),
-                DB::raw('SUM(jumlah) as total_nilai')
-            )
-            ->where('jenis_laporan', '=', 'REVENUE')
-            ->groupBy('tahun', 'bulan')
-            ->orderBy('tahun', 'asc')
-            ->orderBy('bulan', 'asc')
-            ->get();
+            $targetData = DB::table('target')
+                ->select(
+                    DB::raw('tahun as year'),
+                    DB::raw('bulan as month'),
+                    DB::raw('SUM(jumlah) as total_nilai')
+                )
+                ->where('jenis_laporan', '=', 'REVENUE')
+                ->groupBy('tahun', 'bulan')
+                ->orderBy('tahun', 'asc')
+                ->orderBy('bulan', 'asc')
+                ->get();
 
-    $targetData = DB::table('target')
-        ->select(
-            DB::raw('tahun as year'),
-            DB::raw('bulan as month'),
-            DB::raw('SUM(jumlah) as total_nilai')
-        )
-        ->where('jenis_laporan', '=', 'REVENUE')
-        ->groupBy('tahun', 'bulan')
-        ->orderBy('tahun', 'asc')
-        ->orderBy('bulan', 'asc')
-        ->get();
+            $targetData = DB::table('target')
+                ->select(
+                    DB::raw('tahun as year'),
+                    DB::raw('bulan as month'),
+                    DB::raw('SUM(jumlah) as total_nilai')
+                )
+                ->where('jenis_laporan', '=', 'REVENUE')
+                ->groupBy('tahun', 'bulan')
+                ->orderBy('tahun', 'asc')
+                ->orderBy('bulan', 'asc')
+                ->get();
 
-    $account = Auth::guard('account')->user();
-    if ($account->role == "Commerce") {
-        return view('commerce.dashboard.revenue', [
-            "title" => "Revenue",
-            "commerceData" => $commerceData,
-            "revenueData" => $revenueData,
-            "targetData" => $targetData
-        ]);
-    } else {
-        return view('admin.dashboard.revenue', [
-            "title" => "Revenue",
-            "commerceData" => $commerceData,
-            "revenueData" => $revenueData,
-            "targetData" => $targetData,
-            'tahunData' => $tahunData,
-        ]);
-    }    
-}
+            $account = Auth::guard('account')->user();
+            if ($account->role == "Commerce") {
+                return view('commerce.dashboard.revenue', [
+                    "title" => "Revenue",
+                    "commerceData" => $commerceData,
+                    "revenueData" => $revenueData,
+                    "targetData" => $targetData
+                ]);
+            } else {
+                return view('admin.dashboard.revenue', [
+                    "title" => "Revenue",
+                    "commerceData" => $commerceData,
+                    "revenueData" => $revenueData,
+                    "targetData" => $targetData,
+                    'tahunData' => $tahunData,
+                ]);
+            }
+        }
+    }
 }
